e99ff2152acc6bdf02bb0338c2542a17cbf2c0d6
markhitos <mc2622016@gmail.com>
2024-12-18 21:11:16 -0300

    ULTIMO AJUSTE

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css'
 
 function App() {
-  console.log('API URL:', import.meta.env.VITE_VERCEL_URL);
-  const URL = `https://${import.meta.env.VITE_VERCEL_URLL}`;
+  console.log('API URL:', import.meta.env.VITE_APP_URL);
+  const URL = `https://${import.meta.env.VITE_APP_URLL}`;
 
   return (
     <div>
       <h1>Variable de Entorno en React</h1>
-      <p>API URL: {import.meta.env.VITE_VERCEL_URL}</p>
+      <p>API URL: {import.meta.env.VITE_APP_URL}</p>
       <a href={URL}>Ir a la URL</a>
 
     </div>
